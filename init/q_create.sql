ba37402d03fe9a10148c21440c7c1313ee19b2fb
jarle pedersen <jarle.pedersen@norconsult.com>
2019-05-07 11:15:23 +0200

    first commit

---
+++ init/q_create.sql
@@ -0,0 +1,24 @@
+CREATE TABLE public.point
+(
+  id serial,
+  geometry geometry(Point, 4258)
+)
+WITH (
+  OIDS=FALSE
+);
+CREATE TABLE public.line
+(
+  id serial,
+  geometry geometry(LineString, 4258)
+)
+WITH (
+  OIDS=FALSE
+);
+CREATE TABLE public.polygon
+(
+  id serial,
+  geometry geometry(Polygon, 4258)
+)
+WITH (
+  OIDS=FALSE
+);
