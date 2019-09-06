70ecd07132d0f71fa3c031646a8807cd96a1de79
Terje Olsøybakk <to@nois.no>
2019-09-06 11:01:11 +0200

    Attachments

--- init/q_create.sql
+++ init/q_create.sql
@@ -37,7 +37,7 @@ INSERT INTO public.point(geometry, info, beskyttet, skjult)
 VALUES(ST_GeomFromText('POINT(570130 7032300)', 25832), 'Heisann', 'ikke redigerbar', 'usynlig...');
 
 INSERT INTO public.point(geometry, info, beskyttet, skjult, valg, tall, vedlegg)
-VALUES(ST_GeomFromText('POINT(570150 7032350)', 25832), 'Hoppsann', 'ikke redigerbar', 'usynlig...', true, 123, '');
+VALUES(ST_GeomFromText('POINT(570150 7032350)', 25832), 'Hoppsann', 'ikke redigerbar', 'usynlig...', true, 123, 'https://geoinnsyn3.nois.no/servicesmaster/ISY.GIS.IsyAttachment/api/v1/attachment/get?application=gibeta&project=delesaker&file=BodyPart_5c7d445f-79be-4fd5-a209-1428eb61461f.png|https://geoinnsyn3.nois.no/servicesmaster/ISY.GIS.IsyAttachment/api/v1/attachment/get?application=gibeta&project=delesaker&file=BodyPart_e1d435a0-cfe3-4496-9f16-a3805d9f38c1.png');
 
 INSERT INTO public.line(geometry, info)
 VALUES(ST_GeomFromText('LINESTRING(570003.9184570312 7032208.909667968, 570109.0385742188 7032440.967285155, 570377.4584960938 7032440.967285155, 570424.3989257812 7032286.26220703)', 25832), 'Heisann');
